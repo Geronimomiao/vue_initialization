79cd593ec8bbe78bcdda9cd4cfb7ba567f4b66a1
wsm <1030057982@qq.com>
2019-03-01 12:52:30 +0800

    Initial commit

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="nav_footer">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavFooter"
+    }
+</script>
+
+<style lang="stylus">
+
+  #nav_footer
+    position: fixed
+    bottom: 0
+    width: 100%
+    height 3.5rem
+    background-color #999
+    display flex
+    .item
+      flex 1
+
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav-footer></nav-footer>
+</template>
+
+<script>
+  import NavFooter from '../components/NavFooter'
+  export default {
+    name: "index",
+    components: {
+      NavFooter
+    }
+  }
+</script>
+
+<style lang="stylus">
+
+</style>
